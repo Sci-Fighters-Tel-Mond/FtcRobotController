7509c2d4bc4ddf22f18b91d28aa15f6271bcaaca
ROY KAGAN <roiekagan8@gmail.com>
2020-11-01 19:40:06 +0200

    Merge branch 'master' of C:\Users\ROY KAGAN\StudioProjects\UltimateGoal with conflicts.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyOpenCV_KAGAN.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyOpenCV_KAGAN.java
@@ -24,11 +24,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 @TeleOp
-public class EasyOpenCV_KAGAN extends LinearOpMode {
+public class EasyOpenCV_KAGAN extends LinearOpMode
+{
     OpenCvInternalCamera phoneCam;
     BananaPipeline pipeline;
 
-    static private class BananaPipeline extends OpenCvPipeline {
+    static private class BananaPipeline extends OpenCvPipeline
+    {
+
         Mat hsv = new Mat();
         Mat mask = new Mat();
         double ratio = -1;
@@ -48,17 +51,13 @@ public class EasyOpenCV_KAGAN extends LinearOpMode {
                                         //  H    S    V
             Scalar yellow_min = new Scalar(23, 100, 130);
             Scalar yellow_max = new Scalar(55, 255, 255);
-            Scalar blue_min = new Scalar(170, 100, 100);
-            Scalar blue_max = new Scalar(198, 100, 100);
-            Scalar red_min = new Scalar(0, 255, 255);
-            Scalar red_max = new Scalar(12, 255, 255);
             Scalar green_min = new Scalar(80, 255, 255);
             Scalar green_max = new Scalar(103, 223, 255);
 
-            Core.inRange(hsv, yellow_min, yellow_max, mask);
+            Core.inRange(hsv, green_min, green_max, mask);
             frame.setTo(new Scalar(255,123,132), mask);
 
-            ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+            ArrayList <MatOfPoint> contours = new ArrayList<MatOfPoint>();
             Mat her = new Mat();
             Imgproc.findContours(mask, contours, her, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
@@ -85,37 +84,41 @@ public class EasyOpenCV_KAGAN extends LinearOpMode {
     }
 
 
-    private void setupCamera() {
+
+    private void setupCamera()
+    {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
         pipeline = new EasyOpenCV_KAGAN.BananaPipeline();
         phoneCam.setPipeline(pipeline);
 
-        // We set the viewport policy to optimized view so the preview doesn't appear 90 deg
-        // out when the RC activity is in portrait. We do our actual image processing assuming
-        // landscape orientation, though.
         phoneCam.setViewportRenderingPolicy(OpenCvCamera.ViewportRenderingPolicy.OPTIMIZE_VIEW);
 
-        phoneCam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+        phoneCam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+
             @Override
-            public void onOpened() {
-                phoneCam.startStreaming(320,240, OpenCvCameraRotation.UPRIGHT);
-            }
-        });
+            public void onOpened()
+            {phoneCam.startStreaming(320,240, OpenCvCameraRotation.UPRIGHT);}});
+
     }
 
 
 
+
+
     @Override
-    public void runOpMode() {
+    public void runOpMode()
+    {
 
         setupCamera();
 
         waitForStart();
 
-        while (opModeIsActive()) {
-//            telemetry.addData("Analysis", pipeline.getAnalysis());
-//            telemetry.addData("Position", pipeline.position);
+        while (opModeIsActive())
+        {
+            //telemetry.addData("Analysis", pipeline.getAnalysis());
+            //telemetry.addData("Position", pipeline.position);
             telemetry.update();
 
             // Don't burn CPU cycles busy-looping in this sample
