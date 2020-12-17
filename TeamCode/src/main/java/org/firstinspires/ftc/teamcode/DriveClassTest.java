283ac68b1e44c87b0898e7f6fe909b0754c7dec9
ROYKAGAN <roiekagan8@gmail.com>
2020-12-17 19:52:51 +0200

    added DriveClassTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveClassTest.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.util.DriveClass;
+
+
+@TeleOp(group="Test")
+//@Disabled
+public class DriveClassTest extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private DriveClass robot = new DriveClass(this)
+                                        .useEncoders()
+                                        .useBrake();
+
+    @Override
+    public void runOpMode() {
+        robot.init(hardwareMap);
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()) {
+            double boost = gamepad1.left_trigger * 0.5 + 0.5;
+            double drive = -gamepad1.left_stick_y * boost;
+            double turn = gamepad1.right_stick_x * boost;
+            double strafe = gamepad1.left_stick_x * boost;
+
+            robot.setPower(drive, turn, strafe);
+        }
+    }
+}
