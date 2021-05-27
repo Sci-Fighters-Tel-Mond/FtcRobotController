85e9bda5c4a00f51632a0278bf5a2ec3cd6ac843
Itay <naveh.itay@gmail.com>
2021-05-27 20:15:20 +0300

    tabs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cobalt.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cobalt.java
@@ -12,185 +12,168 @@ import org.firstinspires.ftc.teamcode.util.Toggle;
 @TeleOp(group = "Cobalt")
 //@Disabled
 public class Cobalt extends LinearOpMode {
-    final double tile = 0.6;
+	final double tile = 0.6;
 
-    // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();
-    Location startingPosition = new Location(0*tile,0*tile); //last x = -1.75*tile, y = 0*tile
-    private DriveClass drive = new DriveClass(this, DriveClass.ROBOT.COBALT, startingPosition).useEncoders().useBrake();
-    private GameClass game = new GameClass(this);
+	// Declare OpMode members.
+	private ElapsedTime runtime = new ElapsedTime();
+	Location startingPosition = new Location(0*tile,0*tile); //last x = -1.75*tile, y = 0*tile
+	private DriveClass drive = new DriveClass(this, DriveClass.ROBOT.COBALT, startingPosition).useEncoders().useBrake();
+	private GameClass game = new GameClass(this);
 
-    private Toggle reverseIntake = new Toggle();
-    private Toggle wobbleForward = new Toggle();
-    private Toggle wobbleBackward = new Toggle();
-    private Toggle shootHeading = new Toggle();
-    private Toggle ringFire = new Toggle();
-    private Toggle turningToggle = new Toggle();
+	private Toggle reverseIntake = new Toggle();
+	private Toggle wobbleForward = new Toggle();
+	private Toggle wobbleBackward = new Toggle();
+	private Toggle shootHeading = new Toggle();
+	private Toggle ringFire = new Toggle();
+	private Toggle turningToggle = new Toggle();
 
-    private double targetHeading = 0;
+	private double targetHeading = 0;
 
-    @Override
-    public void runOpMode() {
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
+	@Override
+	public void runOpMode() {
+		telemetry.addData("Status", "Initialized");
+		telemetry.update();
 
-        drive.init(hardwareMap);
-        game.init(hardwareMap);
+		drive.init(hardwareMap);
+		game.init(hardwareMap);
 
-        game.initLifterPosition();
-        game.initWobbleArmPosition();
+		game.initLifterPosition();
+		game.initWobbleArmPosition();
 
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
+		// Wait for the game to start (driver presses PLAY)
+		waitForStart();
 
-        drive.resetOrientation(90); //default blue
+		drive.resetOrientation(90); //default blue
 
-        runtime.reset();
+		runtime.reset();
 
-        int turningCount = 0;
+		int turningCount = 0;
 
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
+		// run until the end of the match (driver presses STOP)
+		while (opModeIsActive()) {
 
-            boolean armShooter = gamepad1.x && !gamepad1.start; // up armShooter
-            boolean grabberOpen = gamepad1.y && !gamepad1.start; // open wobble grabbers.
-            boolean grabberClose = gamepad1.b;
-            boolean stopAll = gamepad1.a;
-            boolean intake = gamepad1.dpad_right; // down armShooter
+			boolean armShooter = gamepad1.x && !gamepad1.start; // up armShooter
+			boolean grabberOpen = gamepad1.y && !gamepad1.start; // open wobble grabbers.
+			boolean grabberClose = gamepad1.b;
+			boolean stopAll = gamepad1.a;
+			boolean intake = gamepad1.dpad_right; // down armShooter
 
-            reverseIntake.update(gamepad1.dpad_left);
-            wobbleForward.update(gamepad1.dpad_up);
-            wobbleBackward.update(gamepad1.dpad_down);
-            shootHeading.update(gamepad1.back);
-            ringFire.update(gamepad1.right_bumper);
-
-
-            boolean resetOrientation = gamepad1.start;
-
-            if (resetOrientation) {
-                if (gamepad1.x){
-                    drive.resetOrientation(90);
-                }
-                if(gamepad1.y){
-                    drive.resetOrientation(-90);
-                }
-                drive.resetPosition();
-                targetHeading = drive.getHeading();
-                continue;
-            }
-
-            boolean fieldOriented = !gamepad1.left_bumper;
-            double boost = gamepad1.right_trigger * 0.6 + 0.4;
-
-            double y = -gamepad1.left_stick_y * boost;
-            double x = gamepad1.left_stick_x * boost;
-            double turn = gamepad1.right_stick_x * boost;
-
-            turningToggle.update(Math.abs(turn)>0.05);
-
-            if (turningToggle.isReleased()){
-                turningCount = 5;
-            }
-            if (!turningToggle.isPressed()){
-                turningCount--;
-            }
-
-            if(turningCount == 0){
-                targetHeading = drive.getHeading();
-
-            }
-
-            if (! turningToggle.isPressed() && turningCount < 0){
-                double delta = drive.getDeltaHeading(targetHeading);
-                double gain = 0.05;
-                turn = delta * gain;
-            }
-
-            drive.setPowerOriented(y, x, turn, fieldOriented);
-
-            if (shootHeading.isClicked()) {
-                drive.turnTo(3, 1);
-            }
-
-            if (ringFire.isClicked()) {
-                game.setRingMover(0);
-                sleep(300);
-                game.setRingMover(1);
-            }
-
-            if (wobbleBackward.isClicked()) {
-                game.wobbleArmGoTo(2850);
-               // game.setWobbleArm(-0.6);
-            } //else if (wobbleBackward.isReleased()){
-                //game.setWobbleArm(0.0);
-                //game.wobbleArmGoTo(3000);
-
-           // }
-
-            if (wobbleForward.isClicked()) {
-                game.wobbleArmGoTo(6185);
-                //game.setWobbleArm(0.6);
-            }//} else if (wobbleForward.isReleased()) {
-                //game.setWobbleArm(0);
-            //}
+			reverseIntake.update(gamepad1.dpad_left);
+			wobbleForward.update(gamepad1.dpad_up);
+			wobbleBackward.update(gamepad1.dpad_down);
+			shootHeading.update(gamepad1.back);
+			ringFire.update(gamepad1.right_bumper);
 
 
-            if (grabberOpen) {
-                game.setWobbleGrabber(true);
-            }
+			boolean resetOrientation = gamepad1.start;
 
-            if (grabberClose) {
-                game.setWobbleGrabber(false);
-            }
+			if (resetOrientation) {
+				if (gamepad1.x){
+					drive.resetOrientation(90);
+				}
+				if(gamepad1.y){
+					drive.resetOrientation(-90);
+				}
+				drive.resetPosition();
+				targetHeading = drive.getHeading();
+				continue;
+			}
 
-            if (armShooter) {
-                game.setSuperPosition(true);
-                telemetry.addData("X ", "IS PRESSED");
+			boolean fieldOriented = !gamepad1.left_bumper;
+			double boost = gamepad1.right_trigger * 0.6 + 0.4;
 
-            }
+			double y = -gamepad1.left_stick_y * boost;
+			double x = gamepad1.left_stick_x * boost;
+			double turn = gamepad1.right_stick_x * boost;
 
-            if (intake) {
-                game.setSuperPosition(false);
-            }
+			turningToggle.update(Math.abs(turn)>0.05);
 
-            if (reverseIntake.isPressed()) {
-                game.setIntakePower(-1);
-            } else {
-                if (reverseIntake.isReleased()) {
-                    game.setIntakePower(0);
-                }
-            }
+			if (turningToggle.isReleased()){
+				turningCount = 5;
+			}
+			if (!turningToggle.isPressed()){
+				turningCount--;
+			}
 
-            if (stopAll) {
-                game.stopAll();
-            }
+			if(turningCount == 0){
+				targetHeading = drive.getHeading();
 
-            game.lifterTest(-gamepad1.right_stick_y);
-            telemetry.addData("X Pos",drive.getPosX());
-            telemetry.addData("Y Pos", drive.getPosY());
-            telemetry.addData("Heading", drive.getHeading());
-            telemetry.addData("Target", targetHeading);
-            telemetry.addData("Delta", drive.getDeltaHeading(targetHeading));
+			}
 
-            game.update();
-            telemetry.update();
-        }
-    }
+			if (! turningToggle.isPressed() && turningCount < 0){
+				double delta = drive.getDeltaHeading(targetHeading);
+				double gain = 0.05;
+				turn = delta * gain;
+			}
+
+			drive.setPowerOriented(y, x, turn, fieldOriented);
+
+			if (shootHeading.isClicked()) {
+				drive.turnTo(3, 1);
+			}
+
+			if (ringFire.isClicked()) {
+				game.setRingMover(0);
+				sleep(300);
+				game.setRingMover(1);
+			}
+
+			if (wobbleBackward.isClicked()) {
+				game.wobbleArmGoTo(2850);
+				// game.setWobbleArm(-0.6);
+			} //else if (wobbleBackward.isReleased()){
+			//game.setWobbleArm(0.0);
+			//game.wobbleArmGoTo(3000);
+
+			// }
+
+			if (wobbleForward.isClicked()) {
+				game.wobbleArmGoTo(6185);
+				//game.setWobbleArm(0.6);
+			}//} else if (wobbleForward.isReleased()) {
+			//game.setWobbleArm(0);
+			//}
+
+
+			if (grabberOpen) {
+				game.setWobbleGrabber(true);
+			}
+
+			if (grabberClose) {
+				game.setWobbleGrabber(false);
+			}
+
+			if (armShooter) {
+				game.setSuperPosition(true);
+				telemetry.addData("X ", "IS PRESSED");
+
+			}
+
+			if (intake) {
+				game.setSuperPosition(false);
+			}
+
+			if (reverseIntake.isPressed()) {
+				game.setIntakePower(-1);
+			} else {
+				if (reverseIntake.isReleased()) {
+					game.setIntakePower(0);
+				}
+			}
+
+			if (stopAll) {
+				game.stopAll();
+			}
+
+			game.lifterTest(-gamepad1.right_stick_y);
+			telemetry.addData("X Pos",drive.getPosX());
+			telemetry.addData("Y Pos", drive.getPosY());
+			telemetry.addData("Heading", drive.getHeading());
+			telemetry.addData("Target", targetHeading);
+			telemetry.addData("Delta", drive.getDeltaHeading(targetHeading));
+
+			game.update();
+			telemetry.update();
+		}
+	}
 }
-
-// ;))
-// ;)))
-// ;)))
-// ;)))
-// ;)))
-// ;)))
-// ;)))
-// ;)))
-// ;)))
-// ;)))
-// ;)))
-// ;)))
-// ;)))
-// ;)))
-// ;)))
-// ;)))
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/GameClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/GameClass.java
@@ -10,291 +10,291 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class GameClass {
 
-    private LinearOpMode opMode;
+	private LinearOpMode opMode;
 
-    private DcMotorEx shooter = null;
-    public DcMotorEx lifter = null;
-    private DcMotorEx intake = null;
+	private DcMotorEx shooter = null;
+	public DcMotorEx lifter = null;
+	private DcMotorEx intake = null;
 
-    private DcMotorEx wobbleArm = null;
-    private Servo wobbleGrabber1 = null;
-    private Servo wobbleGrabber2 = null;
-    private DigitalChannel wobbleArmLimiter = null;
-    private DigitalChannel lifterLimiter = null;
+	private DcMotorEx wobbleArm = null;
+	private Servo wobbleGrabber1 = null;
+	private Servo wobbleGrabber2 = null;
+	private DigitalChannel wobbleArmLimiter = null;
+	private DigitalChannel lifterLimiter = null;
 
-    private Servo ringMover = null; // 1 - inside, 0 - outside
+	private Servo ringMover = null; // 1 - inside, 0 - outside
 
-    private Toggle superState = new Toggle();// true - shooterPosition
-    private Toggle shooterState = new Toggle();
-    private Toggle intakeState = new Toggle();
-    private Toggle wobbleGrabberState = new Toggle();
-    private Toggle testLifterToggle = new Toggle();
+	private Toggle superState = new Toggle();// true - shooterPosition
+	private Toggle shooterState = new Toggle();
+	private Toggle intakeState = new Toggle();
+	private Toggle wobbleGrabberState = new Toggle();
+	private Toggle testLifterToggle = new Toggle();
 
-    private enum LifterRequest {UP, DOWN, STAY};
-    private LifterRequest lifterRequest = LifterRequest.STAY;
+	private enum LifterRequest {UP, DOWN, STAY};
+	private LifterRequest lifterRequest = LifterRequest.STAY;
 
-    final private double shooterSpeed = 0.9;
-    final private int lifterUpTargetPosition = 1840;
-    final private int lifterDownTargetPosition = 0;
-
-
-    private ElapsedTime timer = new ElapsedTime();
+	final private double shooterSpeed = 0.9;
+	final private int lifterUpTargetPosition = 1840;
+	final private int lifterDownTargetPosition = 0;
 
 
-    public GameClass(LinearOpMode opMode) {
-        this.opMode = opMode;
-    }
-
-    public void init(HardwareMap hw) {
-        //region get from hw
-        shooter = hw.get(DcMotorEx.class, "shooter");
-        lifter = hw.get(DcMotorEx.class, "lifter");
-        intake = hw.get(DcMotorEx.class, "collector");
-
-        wobbleArm = hw.get(DcMotorEx.class, "wobble");
-        wobbleGrabber1 = hw.get(Servo.class, "wobble_grabber1");
-        wobbleGrabber2 = hw.get(Servo.class, "wobble_grabber2");
-        wobbleArmLimiter = hw.get(DigitalChannel.class, "wobble_limiter");
-        lifterLimiter = hw.get(DigitalChannel.class, "shooter_limiter");
-
-        ringMover = hw.get(Servo.class, "ring_mover");
-        //endregion get from hw
-
-        //region setDirection
-        intake.setDirection(DcMotorEx.Direction.REVERSE);
-        lifter.setDirection(DcMotorEx.Direction.REVERSE);
-
-        wobbleArm.setDirection(DcMotorEx.Direction.REVERSE);
-        //endregion setDirection
-
-        //region encoders
-        shooter.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        wobbleArm.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        lifter.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-        //endregion encoders
-
-        lifter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        ringMover.setPosition(1);
-    }
-
-    public void setSuperPosition(boolean goUp) {
-        if (goUp) {
-            setIntake(false);
-            setShooterRoller(true);
-            lifterUpDown(true); // up
-            if (getWobbleArmPos() <= 400) {
-                wobbleArmGoTo(3000);
-            }
-
-        } else { // goDown
-            setShooterRoller(false); // stop shooter
-            lifterUpDown(false); // down
-        }
-    }
-
-    public boolean getSuperState(){
-        update();
-        return superState.getState();
-    }
-
-    public void lifterUpDown(boolean goUp) {
-        if (goUp) {
-            lifter.setTargetPosition(lifterUpTargetPosition);
-            lifter.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            lifter.setPower(1);
-
-            lifterRequest = LifterRequest.UP;
-        } else {
-            lifter.setTargetPosition(lifterDownTargetPosition);
-            lifter.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            lifter.setPower(1);
-
-            lifterRequest = LifterRequest.DOWN;
-
-        }
-        timer.reset();
-    }
-
-    public void lifterMove(int goUp) {
-        int lifterCurruntPosition = lifter.getCurrentPosition();
-        if (goUp > 0) {
-            lifter.setTargetPosition(lifterCurruntPosition + goUp);
-            lifter.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            lifter.setPower(1);
-
-            //lifterRequest = LifterRequest.UP;
-            opMode.telemetry.addData("lifter going up", goUp);
-        } else {
-            lifter.setTargetPosition(lifterCurruntPosition + goUp);
-            lifter.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            lifter.setPower(1);
-
-            //lifterRequest = LifterRequest.DOWN;
-            opMode.telemetry.addData("lifter going down", goUp);
-        }
-        timer.reset();
-    }
-
-    public void update() {
-        opMode.telemetry.addData("wobble position", getWobbleArmPos());
-
-        opMode.telemetry.addData("Lifter pos", lifter.getCurrentPosition());
-        opMode.telemetry.addData("shooter!!!!!!!!!!!", shooter.getVelocity());
-
-        if (lifterRequest == LifterRequest.UP){
-            if (lifter.getCurrentPosition() > lifterUpTargetPosition  || timer.milliseconds() > 4000 ){
-                lifterRequest = LifterRequest.STAY;
-                superState.set(true);
-            }
-            opMode.telemetry.addData("Lifter GO UP", timer.milliseconds());
-        }
-
-        if (lifterRequest == LifterRequest.DOWN) {
-            if (getLifterLimiter() || (timer.milliseconds() > 4000)) {
-                lifterRequest = LifterRequest.STAY;
-                setIntake(true);
-                lifter.setPower(0);
-                superState.set(false);
-            }
-            opMode.telemetry.addData("Lifter GO Down", timer.milliseconds());
-        }
-
-        opMode.telemetry.addData("Super State", superState.getState());
-    }
-
-    public void lifterTest(double pow) {
-        testLifterToggle.update(Math.abs(pow) > 0.2);
-        if (testLifterToggle.isClicked()) {
-            lifter.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        }
-        if (testLifterToggle.isPressed()) {
-            int curTicks = lifter.getCurrentPosition();
-            if ((curTicks > 2800 && pow > 0) || (getLifterLimiter() && pow < 0)) {
-                pow = 0;
-            }
-            lifter.setPower(pow);
-            opMode.telemetry.addData("TEST Lifter Power", pow);
-        } else if (testLifterToggle.isReleased()) {
-            opMode.telemetry.addData("TEST Lifter Power", pow);
-            lifter.setPower(0);
-        }
-    }
-
-    public void initWobbleArmPosition(){
-        wobbleArm.setPower(-0.6);
-        ElapsedTime time = new ElapsedTime();
-        while (getWobbleArmLimiter() == false){
-           if ( time.milliseconds() > 5000) break;
-        }
-        wobbleArm.setPower(0);
-        opMode.sleep(500);
-        wobbleArm.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        wobbleArm.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-    }
-
-    public int getWobbleArmPos(){
-        return wobbleArm.getCurrentPosition();
-    }
-
-    public void wobbleArmGoTo(int position) {
-        wobbleArm.setTargetPosition(position);
-        wobbleArm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        wobbleArm.setPower(1);
-    }
-
-    public void initLifterPosition() {
-        if (getLifterLimiter() == false) {
-            lifter.setPower(-1);
-            ElapsedTime timer = new ElapsedTime();
-            while (getLifterLimiter() == false) {
-                if (timer.milliseconds() > 4000) break;
-            }
-            lifter.setPower(0);
-            opMode.sleep(1000);
-
-        }
-        lifter.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        lifter.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-
-        superState.set(false);
-    }
+	private ElapsedTime timer = new ElapsedTime();
 
 
-    public boolean getLifterLimiter() {
-        return !lifterLimiter.getState();
-    }
+	public GameClass(LinearOpMode opMode) {
+		this.opMode = opMode;
+	}
+
+	public void init(HardwareMap hw) {
+		//region get from hw
+		shooter = hw.get(DcMotorEx.class, "shooter");
+		lifter = hw.get(DcMotorEx.class, "lifter");
+		intake = hw.get(DcMotorEx.class, "collector");
+
+		wobbleArm = hw.get(DcMotorEx.class, "wobble");
+		wobbleGrabber1 = hw.get(Servo.class, "wobble_grabber1");
+		wobbleGrabber2 = hw.get(Servo.class, "wobble_grabber2");
+		wobbleArmLimiter = hw.get(DigitalChannel.class, "wobble_limiter");
+		lifterLimiter = hw.get(DigitalChannel.class, "shooter_limiter");
+
+		ringMover = hw.get(Servo.class, "ring_mover");
+		//endregion get from hw
+
+		//region setDirection
+		intake.setDirection(DcMotorEx.Direction.REVERSE);
+		lifter.setDirection(DcMotorEx.Direction.REVERSE);
+
+		wobbleArm.setDirection(DcMotorEx.Direction.REVERSE);
+		//endregion setDirection
+
+		//region encoders
+		shooter.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+		wobbleArm.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+		lifter.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+		//endregion encoders
+
+		lifter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+		ringMover.setPosition(1);
+	}
+
+	public void setSuperPosition(boolean goUp) {
+		if (goUp) {
+			setIntake(false);
+			setShooterRoller(true);
+			lifterUpDown(true); // up
+			if (getWobbleArmPos() <= 400) {
+				wobbleArmGoTo(3000);
+			}
+
+		} else { // goDown
+			setShooterRoller(false); // stop shooter
+			lifterUpDown(false); // down
+		}
+	}
+
+	public boolean getSuperState(){
+		update();
+		return superState.getState();
+	}
+
+	public void lifterUpDown(boolean goUp) {
+		if (goUp) {
+			lifter.setTargetPosition(lifterUpTargetPosition);
+			lifter.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+			lifter.setPower(1);
+
+			lifterRequest = LifterRequest.UP;
+		} else {
+			lifter.setTargetPosition(lifterDownTargetPosition);
+			lifter.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+			lifter.setPower(1);
+
+			lifterRequest = LifterRequest.DOWN;
+
+		}
+		timer.reset();
+	}
+
+	public void lifterMove(int goUp) {
+		int lifterCurruntPosition = lifter.getCurrentPosition();
+		if (goUp > 0) {
+			lifter.setTargetPosition(lifterCurruntPosition + goUp);
+			lifter.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+			lifter.setPower(1);
+
+			//lifterRequest = LifterRequest.UP;
+			opMode.telemetry.addData("lifter going up", goUp);
+		} else {
+			lifter.setTargetPosition(lifterCurruntPosition + goUp);
+			lifter.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+			lifter.setPower(1);
+
+			//lifterRequest = LifterRequest.DOWN;
+			opMode.telemetry.addData("lifter going down", goUp);
+		}
+		timer.reset();
+	}
+
+	public void update() {
+		opMode.telemetry.addData("wobble position", getWobbleArmPos());
+
+		opMode.telemetry.addData("Lifter pos", lifter.getCurrentPosition());
+		opMode.telemetry.addData("shooter!!!!!!!!!!!", shooter.getVelocity());
+
+		if (lifterRequest == LifterRequest.UP){
+			if (lifter.getCurrentPosition() > lifterUpTargetPosition  || timer.milliseconds() > 4000 ){
+				lifterRequest = LifterRequest.STAY;
+				superState.set(true);
+			}
+			opMode.telemetry.addData("Lifter GO UP", timer.milliseconds());
+		}
+
+		if (lifterRequest == LifterRequest.DOWN) {
+			if (getLifterLimiter() || (timer.milliseconds() > 4000)) {
+				lifterRequest = LifterRequest.STAY;
+				setIntake(true);
+				lifter.setPower(0);
+				superState.set(false);
+			}
+			opMode.telemetry.addData("Lifter GO Down", timer.milliseconds());
+		}
+
+		opMode.telemetry.addData("Super State", superState.getState());
+	}
+
+	public void lifterTest(double pow) {
+		testLifterToggle.update(Math.abs(pow) > 0.2);
+		if (testLifterToggle.isClicked()) {
+			lifter.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+		}
+		if (testLifterToggle.isPressed()) {
+			int curTicks = lifter.getCurrentPosition();
+			if ((curTicks > 2800 && pow > 0) || (getLifterLimiter() && pow < 0)) {
+				pow = 0;
+			}
+			lifter.setPower(pow);
+			opMode.telemetry.addData("TEST Lifter Power", pow);
+		} else if (testLifterToggle.isReleased()) {
+			opMode.telemetry.addData("TEST Lifter Power", pow);
+			lifter.setPower(0);
+		}
+	}
+
+	public void initWobbleArmPosition(){
+		wobbleArm.setPower(-0.6);
+		ElapsedTime time = new ElapsedTime();
+		while (getWobbleArmLimiter() == false){
+			if ( time.milliseconds() > 5000) break;
+		}
+		wobbleArm.setPower(0);
+		opMode.sleep(500);
+		wobbleArm.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+		wobbleArm.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+	}
+
+	public int getWobbleArmPos(){
+		return wobbleArm.getCurrentPosition();
+	}
+
+	public void wobbleArmGoTo(int position) {
+		wobbleArm.setTargetPosition(position);
+		wobbleArm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+		wobbleArm.setPower(1);
+	}
+
+	public void initLifterPosition() {
+		if (getLifterLimiter() == false) {
+			lifter.setPower(-1);
+			ElapsedTime timer = new ElapsedTime();
+			while (getLifterLimiter() == false) {
+				if (timer.milliseconds() > 4000) break;
+			}
+			lifter.setPower(0);
+			opMode.sleep(1000);
+
+		}
+		lifter.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+		lifter.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
+		superState.set(false);
+	}
 
 
-    private void setShooterRoller(boolean active) {
-        shooterState.set(active);
-        shooter.setPower(active ? shooterSpeed : 0);
-    }
-
-    private void toggleShooter() {
-        boolean state = shooterState.toggle();
-        setShooterRoller(state);
-    }
+	public boolean getLifterLimiter() {
+		return !lifterLimiter.getState();
+	}
 
 
-    private void setIntake(boolean active) {
-        intakeState.set(active);
-        intake.setPower(active ? 1 : 0);
-    }
+	private void setShooterRoller(boolean active) {
+		shooterState.set(active);
+		shooter.setPower(active ? shooterSpeed : 0);
+	}
 
-    public void setWobbleArm(double pow) {
-        if (wobbleArm.getMode() == DcMotor.RunMode.RUN_TO_POSITION) {
-            wobbleArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        }
+	private void toggleShooter() {
+		boolean state = shooterState.toggle();
+		setShooterRoller(state);
+	}
 
-        if (getWobbleArmLimiter()) {
-            pow = Math.max(pow, 0);
-        }
-        if (getWobbleArmPos() >= 10000) {
-            pow = Math.min(pow, 0);
-        }
-        wobbleArm.setPower(pow);
-    }
 
-    public void setWobbleGrabber(boolean state) {
-        wobbleGrabberState.set(state);
-        wobbleGrabber1.setPosition(state ? 1 : 0);
-        wobbleGrabber2.setPosition(state ? 1 : 0);
-    }
+	private void setIntake(boolean active) {
+		intakeState.set(active);
+		intake.setPower(active ? 1 : 0);
+	}
 
-    public void toggleWobbleGrabber() {
-        boolean state = wobbleGrabberState.toggle();
-        setWobbleGrabber(state);
-    }
+	public void setWobbleArm(double pow) {
+		if (wobbleArm.getMode() == DcMotor.RunMode.RUN_TO_POSITION) {
+			wobbleArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+		}
 
-    public boolean getWobbleArmLimiter() {
-        return !wobbleArmLimiter.getState();
-    }
+		if (getWobbleArmLimiter()) {
+			pow = Math.max(pow, 0);
+		}
+		if (getWobbleArmPos() >= 10000) {
+			pow = Math.min(pow, 0);
+		}
+		wobbleArm.setPower(pow);
+	}
 
-    public void setRingMover(double amt) {
-        if (superState.getState()) {
-            ringMover.setPosition(amt);
-        }
-    }
+	public void setWobbleGrabber(boolean state) {
+		wobbleGrabberState.set(state);
+		wobbleGrabber1.setPosition(state ? 1 : 0);
+		wobbleGrabber2.setPosition(state ? 1 : 0);
+	}
 
-    public void shoot() {
-        if (superState.getState() ) {
-            setRingMover(0);
-            opMode.sleep(300);
-            setRingMover(1);
-        }
-    }
+	public void toggleWobbleGrabber() {
+		boolean state = wobbleGrabberState.toggle();
+		setWobbleGrabber(state);
+	}
 
-    public void setIntakePower(double v) {
-        intake.setPower(v);
-    }
+	public boolean getWobbleArmLimiter() {
+		return !wobbleArmLimiter.getState();
+	}
 
-    public void stopAll() {
-        superState.set(false);
-        setShooterRoller(false);
-        setIntake(false);
+	public void setRingMover(double amt) {
+		if (superState.getState()) {
+			ringMover.setPosition(amt);
+		}
+	}
 
-        wobbleArm.setPower(0);
-        lifter.setPower(0);
-    }
+	public void shoot() {
+		if (superState.getState() ) {
+			setRingMover(0);
+			opMode.sleep(300);
+			setRingMover(1);
+		}
+	}
+
+	public void setIntakePower(double v) {
+		intake.setPower(v);
+	}
+
+	public void stopAll() {
+		superState.set(false);
+		setShooterRoller(false);
+		setIntake(false);
+
+		wobbleArm.setPower(0);
+		lifter.setPower(0);
+	}
 }
